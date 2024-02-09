6dcee42a13471f6dfb6298428116ed22f96d9c8d
Andrie-E <stpaul.martinla@gmail.com>
2024-02-09 22:25:49 +0800

    directions and pseudocode

---
+++ news.py
@@ -0,0 +1,11 @@
+# EXPLAINATION: this is a program that summarize news headlines using a webscraper
+
+# Pseudocode
+
+# import needed libraries
+
+# Provide the link for the news website that would be webscraped
+
+# Create an empty list for the website headlines so it will serve as a holder for the headlines
+
+# Extract and print the headlines
