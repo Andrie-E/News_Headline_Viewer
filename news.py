13b3ecb237a0d855a5df4d6b28ad7109e1a551f4
Andrie-E <stpaul.martinla@gmail.com>
2024-02-09 22:41:37 +0800

    added the libraries and url. Also, updated the pseudocode.

--- news.py
+++ news.py
@@ -3,8 +3,15 @@
 # Pseudocode
 
 # import needed libraries
+import requests
+from bs4 import BeautifulSoup
 
 # Provide the link for the news website that would be webscraped
+url = 'https://www.philstar.com/'
+
+# get request
+
+# parse html
 
 # Create an empty list for the website headlines so it will serve as a holder for the headlines
 
